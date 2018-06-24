d11d4011da0414764662fb3c89dfc042be5d6727
Yuval Dagai <Yuval.dagai@ca.com>
2018-06-24 12:57:20 -0700

    add the deployed websocket endpoint env var

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiBaseUrl: 'https://mock-rest-api-ctdphdsgjj.now.sh',
-    graphqlBaseUrl: 'https://graphql-server-yoga-kegairbttj.now.sh/graphql'
+    graphqlBaseUrl: 'https://graphql-server-yoga-kegairbttj.now.sh/graphql',
+    graphqlWSBaseUrl: 'ws://graphql-server-yoga-kegairbttj.now.sh',
 };
