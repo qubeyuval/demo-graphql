7f2fa2b777b4cf23a2b80d6974abe361018b6fd9
Yuval Dagai <Yuval.dagai@ca.com>
2018-06-27 01:30:08 -0700

    update latest deployment info

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiBaseUrl: 'https://mock-rest-api-ctdphdsgjj.now.sh',
-    graphqlBaseUrl: 'https://graphql-server-yoga-kegairbttj.now.sh/graphql',
-    graphqlWSBaseUrl: 'wss://graphql-server-yoga-kegairbttj.now.sh',
-    graphqlPlaygroundUrl: 'https://graphql-server-yoga-kegairbttj.now.sh/playground',
+    graphqlBaseUrl: 'https://graphql-server-yoga-xtwoyjbhil.now.sh/graphql',
+    graphqlWSBaseUrl: 'wss://graphql-server-yoga-xtwoyjbhil.now.sh',
+    graphqlPlaygroundUrl: 'https://graphql-server-yoga-xtwoyjbhil.now.sh/playground',
 };
